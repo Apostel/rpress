9e87455206831f2b3a62767c805d57439439f680
Apostel <paul.apostel@gmail.com>
2012-03-11 10:48:35 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rpress::Application.config.secret_token = '71f80de2f4d77f5157a922b628833d5caa2f58fd99fe297301b7ce9060269a9cab0e7d4dd4f9702858f0a90014774b645026cce6dc61435586dbfe4e50b15b63'
